a3b76e607cfb5d8ec0ee085ae669dbfa07db3d64
Munaim Khan <khanmail2599@gmail.com>
2026-04-25 17:06:14 +0600

    dragon-news(feat): design grid layout, fetch categories

--- practice-project/module46/dragon-news/src/app/(main)/page.js
+++ practice-project/module46/dragon-news/src/app/(main)/page.js
@@ -1,9 +1,35 @@
 import Image from "next/image";
 
-export default function Home() {
+const getCategories = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+  return await res.json();
+}
+
+export default async function Home() {
+
+  const categories = await getCategories();
+  console.log(categories.data.news_category);
+
   return (
-    <>
-      <h2 className="text-red-500">Homepage</h2>
-    </>
+    <div className="grid grid-cols-12 gap-4 container mx-auto">
+      <div className="font-bold text-3xl col-span-3">
+        <ul className="flex flex-col gap-3">
+          Categories
+          {
+            categories.data.news_category.map((category) => {
+              return <li className="bg-slate-100 rounded-md font-bold text-center text-xl" key={category.id}>{category.category_name}</li>
+            })
+          }
+        </ul>
+      </div>
+
+      <div className="font-bold text-3xl bg-green-500 col-span-6">
+        news
+      </div>
+
+      <div className="font-bold text-3xl bg-red-500 col-span-3">
+        icons
+      </div>
+    </div>
   );
 }
